338589375ba6a251fbd7bd923d4c9265e3918331
kumararduino <kumararduino@gmail.com>
2018-09-20 21:06:25 +0530

    a_star completed

--- day-1_a*.py
+++ day-1_a*.py
@@ -1,11 +1,16 @@
+
+#   start => loop(check => plot shortest path => grow)
+
 import numpy as np
 from random import randint as r
 import pygame
-screen_x = 700
-screen_y = 600
+from time import sleep
+screen_x = 1000
+screen_y = 750
 last_child = []
 background = (51,51,51)
-total_cities = 13
+total_cities = 30
+sleep_time = 0.1
 probability_for_second_con = 0.7
 probability_for_third_con = 0.45
 screen = pygame.display.set_mode((screen_x,screen_y))
@@ -13,6 +18,7 @@ class Node:
     def __init__(self,val):
         self.val = val
         self.child = []
+        self.parent = None
 class CITY:
     def __init__(self,i):
         self.i = i
@@ -50,36 +56,32 @@ for i in range(total_cities):
             temp = order[j]
             order[j] = order[j+1]
             order[j+1] = temp
-# print order
-# print cities_dict
 order = [cities_dict[o] for o in order]
-# roads = [ROAD(cities[r(0,total_cities-1)],cities[r(0,total_cities-1)]) for i in range(total_roads)]
 roads = []
-# print len(cities)
 for i in range(total_cities-1):
     roads.append(ROAD(cities[order[i]],cities[order[i+1]]))
-    cities[order[i]].neighbors.append(i+1)
-    cities[order[i+1]].neighbors.append(i)
+    cities[order[i]].neighbors.append(order[i+1])
+    cities[order[i+1]].neighbors.append(order[i])
     if i > 1 and i < total_cities-2:
         if np.random.random() <= probability_for_second_con:
             roads.append(ROAD(cities[order[i]],cities[order[i+2]]))
-            cities[order[i]].neighbors.append(i+2)
-            cities[order[i+2]].neighbors.append(i)
+            cities[order[i]].neighbors.append(order[i+2])
+            cities[order[i+2]].neighbors.append(order[i])
         if np.random.random() <= probability_for_third_con:
             roads.append(ROAD(cities[order[i]],cities[order[i-2]]))
-            cities[order[i]].neighbors.append(i-2)
-            cities[order[i-2]].neighbors.append(i)
-run = True
+            cities[order[i]].neighbors.append(order[i-2])
+            cities[order[i-2]].neighbors.append(order[i])
 
 
 class AGENT:
     def __init__(self):
-        self.cities = []
+        self.path = []
         self.closedSet = []
         self.openSet = [i for i in range(total_cities)]
+        self.found = False
     def connect(self):
-        for i in range(total_cities - 1):
-            pygame.draw.line(screen,(0,255,0),(cities[i].x,cities[i].y),(cities[i+1].x,cities[i+1].y),2)
+        for i in range(len(self.path) - 1):
+            pygame.draw.line(screen,(0,255,0),(cities[self.path[i]].x,cities[self.path[i]].y),(cities[self.path[i+1]].x,cities[self.path[i+1]].y),2)
     def start(self):
         self.tree = Node(order[0])
         self.closedSet.append(order[0])
@@ -89,39 +91,23 @@ class AGENT:
             neighbor.g = np.hypot(neighbor.x-cities[order[0]].x,neighbor.y-cities[order[0]].y)
             neighbor.h = np.hypot(neighbor.x-cities[order[len(order)-1]].x,neighbor.y-cities[order[len(order)-1]].y)
             neighbor.f = neighbor.g + neighbor.h
-            self.tree.child.append(Node(neighbor.i))
-            self.closedSet.append(neighbor.i)
-            self.openSet.remove(neighbor.i)
+            c = Node(neighbor.i)
+            c.parent = self.tree
+            self.tree.child.append(c)
     def grow(self):
-        global last_child
-        last_child = []
-        find_extremes(self.tree)
-        ord_dict = {cities[ch.val].f:j for (ch,j) in zip(last_child,range(len(last_child)))}
-        orde = [cities[ch.val].f for ch in last_child]
-        for i in range(len(orde)):
-            for j in range(len(orde) - i - 1):
-                if orde[j] > orde[j+1]:
-                    temp = orde[j]
-                    orde[j] = orde[j+1]
-                    orde[j+1] = temp
-        spawn = None
-        for o in orde:
-            if last_child[ord_dict[o]].val not in self.closedSet:
-                spawn = last_child[ord_dict[o]]
-                break
-        if spawn == None:
+        if self.spawn == None:
             return False
         else:
-            self.openSet.remove(spawn.val)
-            self.closedSet.append(spawn.val)
-            for neighbor in cities[spawn.val].neighbors:
-                if neighbor.i not in self.closedSet:
-                    neighbor.g = np.hypot(neighbor.x-cities[spawn.val].x,neighbor.y-cities[spawn.val].y)
-                    neighbor.h = np.hypot(neighbor.x-cities[spawn.val].x,neighbor.y-cities[spawn.val].y)
-                    neighbor.f = neighbor.g + neighbor.h
-                    spawn.child.append(Node(neighbor.i))
-                    self.closedSet.append(neighbor.i)
-                    self.openSet.remove(neighbor.i)
+            self.openSet.remove(self.spawn.val)
+            self.closedSet.append(self.spawn.val)
+            for neighbor in cities[self.spawn.val].neighbors:
+                if neighbor not in self.closedSet:
+                    cities[neighbor].g = np.hypot(cities[neighbor].x-cities[self.spawn.val].x,cities[neighbor].y-cities[self.spawn.val].y)
+                    cities[neighbor].h = np.hypot(cities[neighbor].x-cities[self.spawn.val].x,cities[neighbor].y-cities[self.spawn.val].y)
+                    cities[neighbor].f = cities[neighbor].g + cities[neighbor].h
+                    c = Node(neighbor)
+                    c.parent = self.spawn
+                    self.spawn.child.append(c)
         return True
 
     def check(self):
@@ -130,32 +116,75 @@ class AGENT:
         find_extremes(self.tree)
         for i in last_child:
             if i.val == order[total_cities - 1]:
+                self.spawn = i
+                self.found = True
                 return True
+        ord_dict = {cities[ch.val].f:j for (ch,j) in zip(last_child,range(len(last_child)))}
+        orde = [cities[ch.val].f for ch in last_child]
+        for i in range(len(orde)):
+            for j in range(len(orde) - i - 1):
+                if orde[j] > orde[j+1]:
+                    temp = orde[j]
+                    orde[j] = orde[j+1]
+                    orde[j+1] = temp
+        self.spawn = None
+        for o in orde:
+            if last_child[ord_dict[o]].val not in self.closedSet:
+                self.spawn = last_child[ord_dict[o]]
+                break
         return False
 
-
-
-
+    def shortest_path(self):
+        if self.spawn != None:
+            self.path = []
+            sp = self.spawn
+            while sp.parent != None:
+                self.path.append(sp.val)
+                sp = sp.parent
+            self.path.append(sp.val)
+            self.path = self.path[::-1]
+        # return True
 
 
 
 
 a = AGENT()
 a.start()
+'''
+if not a.check():
+    while a.grow():
+        print a.closedSet,a.openSet
+        if a.check():
+            print "sol found"
+            break
+        a.shortest_path()
+
+else:
+    print "b-no sol"
+    print a.openSet,a.closedSet
+a.shortest_path()
+print a.path
+'''
+# exit()
+run = False
+if not a.check():
+    run = True
+
 while run:
+    sleep(sleep_time)
     screen.fill(background)
     for city in cities:
         city.show()
     for road in roads:
         road.show()
-    if not a.check():
-        if not a.grow():
-            print "no solution"
-            run = False
-    else:
-        print "done"
-        run = False
-    # a.connect()
+    a.connect()
+    if not a.found:
+        if a.grow():
+            if a.check():
+                print "solution found"
+            a.shortest_path()
+        else:
+            print "not growing"
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
